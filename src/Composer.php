48cb349421beb34d487e9b134e12c77638bbe5a5
danieletulone <danieletulone.work@gmail.com>
2023-09-23 19:55:29 +0200

    feat: add getInstalledPackages and isPackageInstalled method to Composer service

--- src/Composer.php
+++ src/Composer.php
@@ -2,6 +2,52 @@
 
 namespace Dedecube\Composer;
 
-class Composer
+use Composer\Factory;
+use Composer\IO\NullIO;
+use Illuminate\Support\Facades\Cache;
+use Dedecube\Composer\Contracts\ComposerInterface;
+
+class Composer implements ComposerInterface
 {
+    protected $composer;
+
+    public function __construct()
+    {
+        $this->composer = Factory::create(new NullIO());
+    }
+
+    public function getInstalledPackages(): array
+    {
+        $repo = $this->composer
+            ->getRepositoryManager()
+            ->getLocalRepository();
+
+        return $repo->getPackages();
+    }
+
+    public function isPackageInstalled($packageName): bool
+    {
+        return Cache::rememberForever(
+            $this->cacheKeyForPackage($packageName),
+            function () use ($packageName) {
+                $installedPackages = $this->getInstalledPackages();
+
+                foreach ($installedPackages as $package) {
+                    if ($package->getName() === $packageName) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        );
+    }
+
+    /**
+     * Get the cache key for the given package name.
+     */
+    private function cacheKeyForPackage(string $packageName): string
+    {
+        return "composer.package.$packageName";
+    }
 }
